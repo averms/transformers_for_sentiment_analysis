ba7bb77c5e46291b5d50ab075e4e7e5d1e59fe0d
Aman Verma <amanraoverma@gmail.com>
2025-03-09 01:47:26 -0600

    Add documentation to compute_metrics.

--- src/common.py
+++ src/common.py
@@ -3,6 +3,7 @@ from transformers.trainer_utils import EvalPrediction
 
 
 def compute_metrics(eval_pred: EvalPrediction) -> dict[str, any]:
+    """Compute various metrics and return a dict of metric name to values."""
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return {"accuracy": np.mean(predictions == labels)}
